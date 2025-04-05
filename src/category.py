592f91c11ec2bc1da96e82b32f2e5e966b1e41ab
Egor Farkov <fef84@icloud.com>
2025-04-05 22:26:30 +0800

    Домашняя работа 14.2

--- src/category.py
+++ src/category.py
@@ -6,19 +6,42 @@ class Category:
 
     name: str
     description: str
-    products: list[Product]
+    __products: list[Product] = []
 
     # Переменная на уровне класса по подсчету количества категорий
     category_count = 0
     # Переменная на уровне класса по подсчету количества товаров
     product_count = 0
 
-    def __init__(self, name: str, description: str, products: list[Product]):
+    def __init__(self, name: str, description: str):
         """Метод для инициализации экземпляра класса."""
 
         self.name = name
         self.description = description
-        self.products = products
 
         self.category_count += 1
-        self.product_count += len(products) if products else 0
+
+    @classmethod
+    def add_product(cls, product: Product) -> None:
+        """Класс-метод для добавления продуктов"""
+        for item in cls.__products:
+            if item.name == product.name:
+                item.quantity += product.quantity
+
+                if item.price <= product.price:
+                    item.price = product.price
+                else:
+                    answer_user = input("Согласны на понижение цены, если да, введите: y, если нет: n")
+
+                    if answer_user.lower() == "y":
+                        item.price = product.price
+        cls.product_count += product.quantity
+        cls.__products.append(product)
+
+    @property
+    def products(self) -> str:
+        """Геттер"""
+        list_products = []
+        for product in self.__products:
+            list_products.append(f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.")
+        return "\n".join(list_products)
--- src/product.py
+++ src/product.py
@@ -1,9 +1,12 @@
+from typing import Any
+
+
 class Product:
     """Создан клас продуктов."""
 
     name: str
     description: str
-    price: float
+    __price: float
     quantity: int
 
     def __init__(self, name: str, description: str, price: float, quantity: int):
@@ -11,5 +14,23 @@ class Product:
 
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, dict_attr: dict) -> Any:
+        """Класс-метод новых продуктов"""
+        return Product(**dict_attr)
+
+    @property
+    def price(self) -> float:
+        """Геттер"""
+        return self.__price
+
+    @price.setter
+    def price(self, set_price: float) -> None:
+        """Сеттер, который задает значения цены"""
+        if set_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = set_price
